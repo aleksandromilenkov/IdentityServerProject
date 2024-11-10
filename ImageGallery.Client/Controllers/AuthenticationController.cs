909e2aba3d0059a42231991b6a27d529e90ae542
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-11-10 22:12:31 +0100

    initial commit

---
+++ ImageGallery.Client/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGallery.Client.Controllers
+{
+    [Authorize]
+    public class AuthenticationController : Controller
+    {
+        public async Task Logout()
+        {
+            // Clears the local cookie:
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Redirects to the IDP linked to scheme
+            // "OpenIdConnectsDefaults.AuthenticationScheme" (oidc)
+            // so it can clear its own session/cookie
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
